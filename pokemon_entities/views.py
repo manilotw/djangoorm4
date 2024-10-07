2e58596dc8418c72e60fb4cf679880d5c559ad03
Amirzhan Albariyev <albariev123@gmail.com>
2024-10-07 19:26:36 +0300

    Rename variable

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -29,10 +29,10 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 def show_all_pokemons(request):
     current_time = localtime()
-    pokemons = PokemonEntity.objects.filter(appeared_at__lt=current_time,disappeared_at__gt=current_time)
+    pokemons_entity = PokemonEntity.objects.filter(appeared_at__lt=current_time,disappeared_at__gt=current_time)
     
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
+    for pokemon in pokemons_entity:
         add_pokemon(
             folium_map, pokemon.lat,
             pokemon.lon,
